88413b17c5fa3e328483f17070a2851a7020540c
vagrant <vagrant@kleer-csd-box>
2016-09-23 21:49:25 +0200

    Se corrige feature

--- features/hea.feature
+++ features/hea.feature
@@ -2,5 +2,5 @@ Feature: HEA  Herramienta Educativa en Aprendizaje
 
 Scenario: Bienvendia a la aplicacion
 Given Inica la Aplicacion
-When Inicia el Juego
-Then debe ser "Bienvenidos al HEA!"
+Then debe ser "Bienvenidos"
+And debe ser "HeaD!"
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -2,11 +2,7 @@ Given(/^Inica la Aplicacion$/) do
   visit '/'
 end
 
-When(/^Inicia el Juego$/) do
-  last_response.body.should =~ /#{"Bienvenidos al HEA!"}/m
-end
-
-Then(/^debe ser "(.*?)"$/) do |arg1|
-  last_response.body.should =~ /#{"Bienvenidos al HEA!"}/m
+Then(/^debe ser "(.*?)"$/) do |texto|
+  last_response.body.should =~ /#{texto}/m
 end
 
