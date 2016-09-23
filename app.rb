4f14946c1cd3321e3b78fe0f20b3cd753ef74fdd
vagrant <vagrant@kleer-csd-box>
2016-09-23 21:37:26 +0200

    Se sube .erb

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 require 'sinatra'
 
 get '/' do
-    @nombre= 'Hea!'
+    @@nombre= 'HeaD!'
+    @palabra = ['g','a','t','o']
+    @posicion = 0
+    @letra= ''
     erb :hea
+end
+
+post '/' do
+    @letra = params[:letraSeleccionada]
+    
+    @array = %w(G A T O)
+    @posicion = @array.index(@letra)
+
+    
+
+    erb :hea
+
 end
 
